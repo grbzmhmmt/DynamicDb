516fc98d95d38228632711eb8640dc9be1a0a18a
BerkayGungor <berkaygungor.1996@gmail.com>
2019-04-28 17:59:14 +0300

    Database reading problem fixed

--- DynamicDb/Pages/LoginPage.aspx.cs
+++ DynamicDb/Pages/LoginPage.aspx.cs
@@ -12,16 +12,23 @@ namespace DynamicDb.Pages
         private string userDatabaseName;
         private string dataSourceName= "DESKTOP - 6UQLI0L";
         private string databaseName = "SqlDynamicDbMyUsers";
+        private string dataFileSourceName = "C:\\Github\\DynamicDb\\DynamicDb\\App_Data\\DynamicDatabase.mdf";
 
-        private string sqlConnection = "Data Source=(LocalDB)/MSSQLLocalDB;" +
-            "AttachDbFilename=App_Data/DynamicDatabase.mdf;" +
-            "Integrated Security=True;" +
-            "Trusted_Connection=True;";
+        //private string sqlConnection = "Data Source=(LocalDB)/MSSQLLocalDB;" +
+        //    "AttachDbFilename=App_Data/DynamicDatabase.mdf;" +
+        //    "Integrated Security=True;" +
+        //    "Trusted_Connection=True;";
 
+        private string sqlConnection;
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            sqlConnection = "Data Source=(LocalDB)\\MSSQLLocalDB;" +
+            "AttachDbFilename=" + dataFileSourceName + ";" +
+            "Initial Catalog=Alpha;" +
+            "Integrated Security=True;" +
+            "Connect Timeout=30;" +
+            "Application Name=DynamicDb";
         }
 
         protected void LoginSystem(object sender, EventArgs e)
@@ -49,18 +56,25 @@ namespace DynamicDb.Pages
                 {
                     userId = dr["UserId"].ToString();
                     //Kullanıcı nın Database adı için sorgu
-                    string query1 = "select DatabaseName from UsersDatabase where UserId= '" + userId + "' and UserName='" + userName + "' and UserPass='" + userPassword + "'";
-                    SqlCommand cmd1 = new SqlCommand(query1, conn);
+                    //string query1 = "select DatabaseName from UsersDatabase where UserId= '" + userId + "' and UserName='" + userName + "' and Password='" + userPassword + "'";
+                    //SqlCommand cmd1 = new SqlCommand(query1, conn);
+                    query = "SELECT user FROM " + "Users" + " WHERE UserId = '" + userId + "' AND UserName = '" + userName + "' AND Password = '" + userPassword + "'";
+
+                    cmd = new SqlCommand(query, conn);
 
                     dr.Close();
-                    SqlDataReader dr1 = cmd1.ExecuteReader();
+                    //SqlDataReader dr1 = cmd1.ExecuteReader();
+                    dr = cmd.ExecuteReader();
+
                     string directAddress="";
 
                     //Kullanıcı nın Database adı olmasına göre dashboarda parametre gönderiliyor
-                    if (dr1.Read())
+                    if (dr.Read()) //(dr1.Read())
                     {
-                        userDatabaseName = dr1["DatabaseName"].ToString();
-                        directAddress = "Dashboard.aspx?" + "userId=" + userId + "&userName=" + userName + "&password=" + userPassword + "&databaseName= " + userDatabaseName;
+                        //userDatabaseName = dr1["DatabaseName"].ToString();
+                        string user = dr["user"].ToString();
+                        //directAddress = "Dashboard.aspx?" + "userId=" + userId + "&userName=" + userName + "&password=" + userPassword + "&databaseName= " + userDatabaseName;
+                        directAddress = "Dashboard.aspx?" + "userId=" + userId + "&userName=" + userName + "&password=" + userPassword + "&databaseName= " + user;
                     }
                     else
                     {
